d9e7db0292e733047d35feea78ec48cb14e440fb
Jacob Alebiosu <jacobalebiosu@gmail.com>
2023-05-15 19:56:43 -0400

    first cycle, slicing

---
+++ practice.py
@@ -0,0 +1,48 @@
+# This file contains practice sessions, random codes
+# Tests, ideas, etc.
+
+
+# Hello world
+"""
+print("hello world")
+"""
+
+# Variable declaration
+"""
+int_var = 7 * 2
+flo_var = 5.6
+bol_var = False
+int_var = 15 / 3 * 2 * 2 - (3 + 4)
+
+print(int_var, flo_var, bol_var)
+"""
+
+# Round
+"""
+print((123 + 280)/100)
+ter = (2.1/4.1)
+print(round(ter, 5))
+#print(round(4, ter)) #this is a wrong attempt.
+
+# I will never let you go
+"""
+
+# String slicing
+"""
+mod3 = "mangowater"
+ind = mod3 [3]
+print(ind)
+print(mod3[:3])
+print(mod3[3:7])
+print(mod3[5:])
+
+nike = "Just do it!"
+print(nike[10])
+print(nike[5:8])
+print(nike[8:])
+print(nike[:4])
+print("Don't " + nike[5:])
+"""
+
+
+
