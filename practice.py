a9c6210cbc24a49b9a0714cef1628631436b9710
Jacob Alebiosu <jacobalebiosu@gmail.com>
2023-05-16 23:26:40 -0400

    get_type, string_ops, vol_calc

--- practice.py
+++ practice.py
@@ -44,5 +44,59 @@ print(nike[:4])
 print("Don't " + nike[5:])
 """
 
+# Get Type
+"""
+g = "4.5"
+print(type(nike))
+print(type(g))
+print('"Hello, I am Jacob, it\'s nice to meet you"')
+print("******* \n ***** \n  ***\n   *")
 
+#name = input("Ogbeni, enter your name:")
+#print("Your name is "+name+".")
+#print(type(name))
+
+sum = int(input("Enter a number:"))
+print(sum+10)
+print(type(sum))
+"""
+
+# String operations with functions
+"""
+first_str = "The number "
+
+def fxn_name(p1, p2, p3):
+    print(p1 + str(p2) + p3)
+
+
+fxn_name(first_str, 5, " is an int")
+
+
+def hello_world_printer():
+    print("Hello world")
+
+
+hello_world_printer()
+
+
+def name_printer(yname):
+    print("Your name is " + yname)
+
+
+name = input("Enter your name:")
+name_printer(name)
+"""
+
+
+# Calculate volume
+"""
+def rec_vol(length, width, height):
+    return length * width * height
+
+
+l = int(input("Length:"))
+w = int(input("Width:"))
+h = int(input("Height:"))
+print("The volume of the rectangular prism is " + str(rec_vol(l,w,h)))
+"""
 
