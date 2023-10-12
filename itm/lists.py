c520e6286f293de3eff6b7f44343009dfc7e58e2
Jacob Alebiosu <jacobalebiosu@gmail.com>
2023-10-11 20:58:23 -0400

    append & insert

--- itm/lists.py
+++ itm/lists.py
@@ -1,22 +1,40 @@
 # Lists are ordered, mutable data structures that allow duplicate element
 
 myList = ["ball", "cherry", "comb"]
-print(myList)
+print(myList2)
 
+"""
 # Lists can contain elements of different data types
 myList2 = [5, True, 45.3, "Bread"]
 print(myList2)
 
 # to ndexe items in the list
 item = myList[0] # where zero is the first and the last is n-1
-print(item)
+#print(item)
 
 # list index out of range error is return when the index is larger than the list
 #print(myList[3])
 
 # prints last item. -2 for 2nd to last, etc.
-print(myList[-1])
+#print(myList[-1])
 
 # iterate over list
 for i in myList2:
     print(i)
+
+# to check if an element is in a list
+if "Breader" in myList:
+    print(True)
+else:
+    print(False)
+
+print(len(myList)) #returns the length of the list
+
+
+myList.append("lemon") #append/add item to the end of the list
+print(myList)
+"""
+
+# to insert item at specific location
+myList.insert(1, "berry") # 1 here is the index position
+print(myList)
