576be2c1839b8ff21cc7ed803335f11b45b56d23
Jacob Alebiosu <jacobalebiosu@gmail.com>
2023-10-11 20:43:29 -0400

    data types and index

--- itm/lists.py
+++ itm/lists.py
@@ -3,3 +3,12 @@
 myList = ["ball", "cherry", "comb"]
 print(myList)
 
+# Lists can contain elements of different data types
+myList2 = [5, True, 45.3, "Bread"]
+print(myList2)
+
+# to ndexe items in the list
+item = myList[0] # where zero is the first and the last is n-1
+print(item)
+
+# list index out of range error is return when the index is larger than the list
