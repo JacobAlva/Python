f0556e1c2c0167043cac2c0f8bbfdce6e2d45609
Jacob Alebiosu <jacobalebiosu@gmail.com>
2023-10-14 09:27:22 -0400

    tuple / strings

--- itm/tuples.py
+++ itm/tuples.py
@@ -1,2 +1,12 @@
 # Tuples: ordered, immutable data structures, allow duplicate elements
 
+mytuple = "Max", 28, "Bread" #this is a tuple
+print(type(mytuple))
+
+#this is a string
+tuple1 = ("Max") # a single element tuple like this is recognised as a string. To make it a tuple, add a comma ("Max",)
+print(type(tuple1))
+
+#this is a tuple
+tuple2 = ("Max",)
+print(type(tuple2)) 
