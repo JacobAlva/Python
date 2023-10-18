69798513454cf40aacd46dc1b387b62ea3fa592d
Jacob Alebiosu <jacobalebiosu@gmail.com>
2023-10-18 01:21:48 -0500

    get element index

--- itm/lists.py
+++ itm/lists.py
@@ -13,7 +13,7 @@ myList2 = [5, True, 45.3, "Bread"]
 item = myList[0] # where zero is the first and the last is n-1
 #print(item)
 
-# list index out of range error is return when the index is larger than the list
+# list "index out of range" error is return when the index is larger than the list
 #print(myList[3])
 
 # prints last item. -2 for 2nd to last, etc.
