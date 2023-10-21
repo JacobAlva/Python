3b95caec0922a21645c949061ff476911c6dad7e
Jacob Alebiosu <jacobalebiosu@gmail.com>
2023-10-20 22:33:56 -0500

    creating a set

---
+++ itm/sets.py
@@ -0,0 +1,14 @@
+# Sets: unordered, mutable data structure, no duplicates
+
+# set is made with curly braces
+set1 = {5,1,3,4,2,2,3}
+print(type(set1))
+print(set1)
+
+# using the set() constructor
+set2 = set("Hello")
+
+#list to set
+myList = ["ball", "cherry", "ball", "comb"]
+set3 = set(myList)
+print(set3)
