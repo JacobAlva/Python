2f7127509ba763d140fa654bcb09384d962fe9a5
Jacob Alebiosu <jacobalebiosu@gmail.com>
2023-10-25 00:20:52 -0500

    multi line strings

---
+++ itm/strings.py
@@ -0,0 +1,12 @@
+# Strings: ordered, immutable, used for text representation
+
+# A string is enclosed in single, double or triple quotes
+# Multi-line string
+string1 = """This is 
+a string"""
+print(string1)
+
+# if you want them printed out on the same line, use an escape
+string2 = """This is a \
+    multi line on one"""
+print (string2)
