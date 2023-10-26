495f5fefafc46dc07318fdfe13d762db45cdfde6
Jacob Alebiosu <jacobalebiosu@gmail.com>
2023-10-25 23:04:28 -0500

    string indexing & assignment

--- itm/strings.py
+++ itm/strings.py
@@ -1,12 +1,27 @@
 # Strings: ordered, immutable, used for text representation
 
 # A string is enclosed in single, double or triple quotes
+
+# regular string
+string0 = "This is a string"
+string0a = 'This is also a string'      # single or multi quotes
+
 # Multi-line string
-string1 = """This is 
-a string"""
+string1 = """This is a
+multiline string"""
 print(string1)
 
 # if you want them printed out on the same line, use an escape
 string2 = """This is a \
-    multi line on one"""
+multi line string on one line"""
 print (string2)
+
+# string indexing
+# A string share a number of similarities with a tuple
+string3 = "Hello World"
+#print([i+"4" for i in string3])
+print(string3[2])   # third character
+print(string3[-1])  # last character
+
+# string assignment
+string3[0] = "h"    # this operation cannot work because a string is immutable
