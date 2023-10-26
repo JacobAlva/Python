91a45293736ee2daca9da67a0a5de326f9d71937
Jacob Alebiosu <jacobalebiosu@gmail.com>
2023-10-25 23:32:47 -0500

    string slicing

--- itm/strings.py
+++ itm/strings.py
@@ -1,7 +1,6 @@
 # Strings: ordered, immutable, used for text representation
 
 # A string is enclosed in single, double or triple quotes
-
 # regular string
 string0 = "This is a string"
 string0a = 'This is also a string'      # single or multi quotes
@@ -9,13 +8,14 @@ string0a = 'This is also a string'      # single or multi quotes
 # Multi-line string
 string1 = """This is a
 multiline string"""
-print(string1)
+#print(string1)
 
 # if you want them printed out on the same line, use an escape
 string2 = """This is a \
 multi line string on one line"""
-print (string2)
+#print (string2)
 
+"""
 # string indexing
 # A string share a number of similarities with a tuple
 string3 = "Hello World"
@@ -25,3 +25,8 @@ print(string3[-1])  # last character
 
 # string assignment
 string3[0] = "h"    # this operation cannot work because a string is immutable
+"""
+# string slicing - to access a substring
+string4 = "welcome"
+print(string4[1:6:2])     # e-m with step NOTE: index 6 "e" is excluded
+print(string4[::-1 ])     # reverses the string
