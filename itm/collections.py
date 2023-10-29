3b46a885e73aca907b339e305c4f96e61fdeef88
Jacob Alebiosu <jacobalebiosu@gmail.com>
2023-10-29 18:49:29 -0500

    named tuple

--- itm/collections.py
+++ itm/collections.py
@@ -1,12 +1,13 @@
 # colelctions: counter, namedtuple, orderedDict, defaultdict, deque
 
-"""
+'''
     implements special container data types and provides alternatives
     to additional funtionalities compared to general built in containers 
     like dictionaries, lists or tuples.
-"""
+'''
 # this section reviews 5 diff types of collections
 
+"""
 # COUNTER
 '''A counter is a container that stores elements as dictionary keys and
     their counts as dictionary values'''
@@ -25,4 +26,9 @@ print(counter1.most_common())           # returns all items sorted in desc order
 print(counter1.most_common(2))          # returns a list of the top 2 most common element enclosed in tuples
 print(counter1.most_common(2)[0][0])    # selects the first element (key) of the first tuple in the list which is the value of the most common element in the collection
 print(list(counter1.elements()))        # a list with all different elements repeating as many times as they count
+"""
 
+# NAMED TUPLE
+'''Easy to create light-weight object type similar to a struct'''
+
+from collections import namedtuple
