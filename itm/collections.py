9aa7ab2f80962232c335b419ac6d6ee7cd6c3fbd
Jacob Alebiosu <jacobalebiosu@gmail.com>
2023-10-28 21:38:38 -0500

    collections

---
+++ itm/collections.py
@@ -0,0 +1,10 @@
+# colelctions: counter, namedtuple, orderedDict, defaultdict, deque
+
+"""
+    implements special container data types and provides alternatives
+    to additional funtionalities compared to general built in containers 
+    like dictionaries, lists or tuples.
+"""
+# this section reviews 5 diff types of collections
+
+
