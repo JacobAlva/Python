ae38bbf5859912a36e8179fc0163dcc7a0109da9
Jacob Alebiosu <jacobalebiosu@gmail.com>
2023-11-05 16:26:18 -0600

    itertools - permutations

--- itm/itertools.py
+++ itm/itertools.py
@@ -4,11 +4,19 @@
 # iterators are datatypes that can be used in a for loop. Most common is a list.
 # Itertools offers some advanced iterator tools 
 
+"""
 from itertools import product
 a = [1,2]
 b = [3]
 prod = product(a,b)     # returns a unique combination of all elements 
 prod = product(a,b, repeat = 2)     # product with repitition # times
 print(list(prod))       
+"""
 
+from itertools import permutations
+# returns all possible orderings of an input
+a = [1,2,3]
+perm = permutations(a)
+print(perm)         # this retunrs an object from the library. Use an iterable (e.g. list to print out the output
+print(list(perm), len(list(perm)))
 
