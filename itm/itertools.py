453c153986a42859c79d0a40139af32c4b16c26f
Jacob Alebiosu <jacobalebiosu@gmail.com>
2023-11-04 23:29:53 -0500

    itertools: product

--- itm/itertools.py
+++ itm/itertools.py
@@ -3,3 +3,9 @@
 # collection of tools for handling iterators. They are datatypes that can be used in a for loop.
 # iterators are datatypes that can be used in a for loop. Most common is a list.
 # Itertools offers some advanced iterator tools 
+
+from itertools import product
+a = [1,2]
+b = [3,4]
+prod = product(a,b)
+print(list(prod))       # combines all elements 
