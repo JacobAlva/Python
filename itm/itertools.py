2e72618480833b2095f465d7737f8252279ca494
Jacob Alebiosu <jacobalebiosu@gmail.com>
2023-11-04 23:24:48 -0500

    notes on itertools

--- itm/itertools.py
+++ itm/itertools.py
@@ -1,2 +1,5 @@
 # itertools: product, permutations, combinations, accumulate, groupby, and infinite iterators
 
+# collection of tools for handling iterators. They are datatypes that can be used in a for loop.
+# iterators are datatypes that can be used in a for loop. Most common is a list.
+# Itertools offers some advanced iterator tools 
