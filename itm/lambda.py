748f7e279c2a2191736088d04a3225f099c8ae2a
Jacob Alebiosu <jacobalebiosu@gmail.com>
2023-11-06 01:19:56 -0600

    sorting with lambda

--- itm/lambda.py
+++ itm/lambda.py
@@ -14,3 +14,24 @@ print(func_add10(5))
 mult = lambda x,y: x*y
 print(mult(2,7))
 
+# lambda usecases
+# list sorting
+points2D = [(1,2), (15,1), (5,-1), (10,4)]
+points2D_sorted = sorted(points2D)      # returns a new sorted list without modifying the original
+print(points2D)
+print(points2D_sorted)      # by default, the list is sorted by the first index
+
+# to sort by a different index would normally take a whole function
+def sort_by_y(x):
+    return x[1]
+points2D_sorted_dfwf = sorted(points2D, key = sort_by_y)
+print(points2D_sorted_dfwf)
+
+# same result with a single line of lambda
+points2D_sorted_df = sorted(points2D, key = lambda x:x[1])
+print(points2D_sorted_df)
+
+
+# to sort by sum of each tuple
+p2D_sorted_sum = sorted(points2D, key = lambda x: x[0] + x[1], reverse=True)    # returns the sort in reverse order
+print(p2D_sorted_sum)
