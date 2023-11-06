5fb2e7d9cd66866f1d442141248309f90615e9e8
Jacob Alebiosu <jacobalebiosu@gmail.com>
2023-11-06 00:32:43 -0600

    lambda functions

---
+++ itm/lambda.py
@@ -0,0 +1,11 @@
+# lambda arguments: expression
+''' Small, single-line anonymous function defined without a name.
+    It starts with the 'lambda', takes in arguments then the expression. '''
+
+add10 = lambda x: x + 10
+print(add10(5))
+
+# this is similar to a function to add 10, but shorter
+def func_add10(x):
+    return x + 10
+print(func_add10(5))
