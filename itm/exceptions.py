ee97ed38b2dfe3ac53aa7ffa1d33fa5ef085b0ff
Jacob Alebiosu <jacobalebiosu@gmail.com>
2023-11-07 02:11:53 -0600

    errors and exceptions

---
+++ itm/exceptions.py
@@ -0,0 +1,21 @@
+# Errors and Exceptions
+# Syntax errors, diff between syntax errors and exceptions, raising and handling exceptions, # defining your own exceptions, common types of errors, ...
+
+# SYNTAX ERROR
+''' Occurs when the parser detects a syntactically incorrect statement.'''
+# a = 5 print(a))       # there are two syntax errors here
+
+''' A syntactically correct statement might still raise an exception error. There are
+    different types defined in this class e.g. TypeError, NameError, IndexError, IOError, etc.
+'''
+
+# a = 5 + "5"     # TypeError: unsupported operand
+
+# import somemodule   # ModuleNotFoundError - subclass from the ImportError
+
+
+
+
+
+
+
