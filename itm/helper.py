1114f4cdf0b787b0a391ede3ed2d5f6fa26c5315
Jacob Alebiosu <jacobalebiosu@gmail.com>
2023-11-11 00:11:55 -0600

    custom logger

--- itm/helper.py
+++ itm/helper.py
@@ -1,8 +1,4 @@
 import logging
 
-logging.basicConfig(level = logging.DEBUG, format = '%(asctime)s - %(name)s - %(levelname)s', datefmt='%m/%d/%Y %H:%M:%S')
-logging.critical('This is a critical message')
-logging.debug('This is a debug message')
-logging.warning('This is a warning message')
-logging.error('This is an error message')
-logging.info('This is an info message')
+# create your custome logger
+logger = logging.getLogger(__name__)
