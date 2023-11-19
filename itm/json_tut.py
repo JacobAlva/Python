2e54b06a7246501f5c02e7581844747ae6d4f1b3
Jacob Alebiosu <jacobalebiosu@gmail.com>
2023-11-18 23:27:32 -0600

    converting from json to python

--- itm/json_tut.py
+++ itm/json_tut.py
@@ -9,3 +9,19 @@ person = {"name": "Alice", "age": 30, "city": "New York", "hasChildren": False, 
 personJSON = json.dumps(person, indent=4, sort_keys=True) # separators=(';', ' = ')) # separators are not recommended, just use the default.
 print(person)
 print(personJSON)
+
+# working with files
+# this writes the content of the person dictionary to file in json format
+with open ('person.json', 'w') as file:
+    json.dump(person, file, indent=4)
+
+
+# Deserialization / Decoding
+# The process of coverting a json data to python object
+person1 = json.loads(personJSON)
+print(person1)      # python object.
+
+# to covnert froma JSON file
+with open ('person.json', 'r') as file:
+    person = json.load(file)
+    print(person)
