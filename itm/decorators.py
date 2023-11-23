4246d71a9cf1e218fed697fc7f29598552959ed1
Jacob Alebiosu <jacobalebiosu@gmail.com>
2023-11-23 00:13:57 -0600

    decorators

---
+++ itm/decorators.py
@@ -0,0 +1,7 @@
+'''
+    A decorator is a function that take another function as argument and extends the behaviour of the function without completely modifying it, thus allowing you to add new functionalities to the existing function.
+'''
+
+@mydecorator
+def dosomething():
+    pass
