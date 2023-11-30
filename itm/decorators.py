9fbaf31eb97912ee042674250f0418af0248e0bd
Jacob Alebiosu <jacobalebiosu@gmail.com>
2023-11-30 00:08:32 -0600

    nexted decorators

--- itm/decorators.py
+++ itm/decorators.py
@@ -69,7 +69,6 @@ print(add5.__name__)
     return wrapper
 '''
 
-"""
 
 # decorators with arguments
 def repeat(num_times):
@@ -87,5 +86,38 @@ def repeat(num_times):
 def greet(name):
     print(f'Hello {name}')
 
-
 greet('Jacob')
+"""
+
+# Nexted decorators or Multiple Decorators
+def startend_dec(func):
+    def wrapper(*args, **kwargs):
+        print('Start')
+        result = func(*args, **kwargs)
+        print('End')
+        return result
+    return wrapper
+
+def debug(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        args_repr = [repr(a) for a in args]
+        kwargs_repr = [f"{k} = {v!r}" for k, v in kwargs.items()]
+        signature = ", ".join(args_repr + kwargs_repr)
+        print(f"Calling {func.__name__}({signature})")
+        result = func(*args, **kwargs)
+        print(f'{func.__name__!r} returned {result!r}')
+        return result
+    return wrapper
+
+
+@debug
+@startend_dec
+def say_hello(name):
+    greeting = f'Hello {name}'
+    print(greeting)
+    return greeting
+
+say_hello('Alex')
+
+
