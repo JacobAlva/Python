6b35294d15c707c35851d4de8b9fe70e74b53d6d
Jacob Alebiosu <jacobalebiosu@gmail.com>
2023-12-11 01:47:25 -0600

    sharing a queue between multiple processes

--- itm/multiproc.py
+++ itm/multiproc.py
@@ -42,7 +42,7 @@
 '''
 
 
-from multiprocessing import Process, Value, Array, Lock
+from multiprocessing import Process, Value, Array, Lock, Queue
 import os
 import time
 
@@ -163,6 +163,7 @@ if __name__ == "__main__":
 """
 
 # sharing an array with multiple processes
+"""
 def add_100(numbers, lock):
     for i in range(100):
         time.sleep(0.01)
@@ -187,3 +188,31 @@ if __name__ == "__main__":
     p2.join()
 
     print('Array at end is', shared_array[:])
+"""
+
+
+# sharing a queue between multiple processes
+def square(numbers, queue):
+    for i in numbers:
+        queue.put(i*i)
+        
+def make_negative(numbers, queue):
+    for i in numbers:
+        queue.put(-1*i)
+
+if __name__ == "__main__":
+
+    numbers = range(1,6)
+    q = Queue()
+
+    p1 = Process(target=square, args=(numbers, q))
+    p2 = Process(target=make_negative, args=(numbers, q))
+
+    p1.start()
+    p2.start()
+
+    p1.join()
+    p2.join()
+
+    while not q.empty():
+        print(q.get())
