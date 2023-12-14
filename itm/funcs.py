c8090c9ccd3b8a8e19d3f860fd15f178d0d5514e
Jacob Alebiosu <jacobalebiosu@gmail.com>
2023-12-13 22:30:26 -0600

    notes

--- itm/funcs.py
+++ itm/funcs.py
@@ -1,5 +1,17 @@
 """
     Functions
 
+    - The difference between arguments and parameters
+        Parameters are variables that are defined or used inside parenthesis while defining a function, while arguments are the values passed for the parameters whiile calling a function. 
+        DPCA - define(parameters)/call(arguments)
+            def some_func(parameters)
+            some_func(argument)
+
+    - Positional and keyword arguments 
+    - Default arguments
+    - Variable-length arguments (*args and **kwargs)
+    - Container unpacking into function arguments 
+    - Local vs. global arguments
+    - Parameter passing (by value or by reference?)
 """
 
