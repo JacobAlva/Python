4a329e3984fdbd6bec2fbfbd82a1ad8eafb2a78d
Jacob Alebiosu <jacobalebiosu@gmail.com>
2023-12-16 10:34:39 -0600

    asterisk - mul, exponential, iterables

---
+++ itm/asterisk.py
@@ -0,0 +1,19 @@
+''' ASTERISK (*)
+    The asterisk is a very important symbol in Python with multiple uses including... Which would be explored below.
+'''
+
+# simple multiplication
+print(5 * 7)
+print(5 ** 7)   # power operation / exponential
+
+# to create iterables (list, tuples, strings) with repeated elements
+rep = [1] * 5
+print(rep)
+rep = [1,2,3] * 5
+print(rep)
+rep = (1) * 5
+print(rep)
+rep = 'Paw' * 2
+print(rep)
+
+# for the *args & **kwargs arguments
