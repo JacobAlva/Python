564559c3cf302bf35226fe314e4b53b8323fe981
Jacob Alebiosu <jacobalebiosu@gmail.com>
2023-12-19 16:11:52 -0600

    thread locking

--- itm/contextmng.py
+++ itm/contextmng.py
@@ -3,6 +3,7 @@
 
 
 # file management
+"""
 with open('ctxnotes.txt', 'w') as file:
     file.write('This is a file context manager')
 
@@ -15,3 +16,19 @@ finally:              # executes with or without an exception
     file.close()
 
 # As can be seen, the "with open" context manager provides a cleaner and more efficient code
+"""
+
+# Thread Locking
+
+from threading import Lock
+lock = Lock()
+
+lock.acquire()
+# do something
+lock.release()      # you must say 
+
+
+# using context manager
+with lock:
+    # do something
+    a = 1
