a8f35d3555aeee1aab42c49b563709000e4bb952
Jacob Alebiosu <jacobalebiosu@gmail.com>
2023-12-19 00:13:34 -0600

    'with open' for file operation

--- itm/contextmng.py
+++ itm/contextmng.py
@@ -1,2 +1,17 @@
 # Context managers
+# Great tool for resource management allowing you to allocate and release resources precisely as required.
 
+
+# file management
+with open('ctxnotes.txt', 'w') as file:
+    file.write('This is a file context manager')
+
+
+# regular way to manage a file
+file = open('ctxnotes.txt', 'w')
+try:
+    file.write('This is a file operation.')
+finally:              # executes with or without an exception
+    file.close()
+
+# As can be seen, the "with open" context manager provides a cleaner and more efficient code
